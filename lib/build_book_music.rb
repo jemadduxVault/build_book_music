2554f21f7d4b4b8f8e6cb813d780a56345040d41
jemaddux <jemaddux@gmail.com>
2023-06-11 16:03:46 -0400

    Loads and writes json data. Changes file also created but no changes are written. Also the file name is ignored for now.

--- lib/build_book_music.rb
+++ lib/build_book_music.rb
@@ -1,7 +1,31 @@
+require "json"
+
 class BuildBookMusic
   def self.call(args)
-    message = "#{args.to_s}"
-    puts message
-    return message
+    # load_changes
+    save_output
+    # puts message
+    # return message
+  end
+
+  def self.load_data
+    file = File.read("spotify.json")
+    data = JSON.parse(file)
+    puts data.to_s
+    return data
+  end
+  
+  def self.load_changes
+    file = File.read("changes.json")
+    data = JSON.parse(file)
+  end
+
+  def self.save_output
+    json_string = JSON.pretty_generate(load_data)
+
+    output_file_path = "output.json"
+    File.open(output_file_path, "w") do |file|
+      file.write(json_string)
+    end
   end
 end
--- test/test_build_book_music.rb
+++ test/test_build_book_music.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require "build_book_music"
 
 class BuildBookMusicTest < Minitest::Test
-  def test_call
-    assert_equal ["1.json", "2.json", "3.json"].to_s, BuildBookMusic.call(["1.json", "2.json", "3.json"])
-  end
+  # def test_call
+  #   assert_equal ["1.json", "2.json", "3.json"].to_s, BuildBookMusic.call(["1.json", "2.json", "3.json"])
+  # end
 end
