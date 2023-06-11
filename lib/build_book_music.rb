299af7863808277d6fea4c7cae85b52fefe6aa83
jemaddux <jemaddux@gmail.com>
2023-06-11 16:16:51 -0400

    Mild validations for inputs.

--- lib/build_book_music.rb
+++ lib/build_book_music.rb
@@ -2,30 +2,39 @@ require "json"
 
 class BuildBookMusic
   def self.call(args)
-    # load_changes
-    save_output
-    # puts message
-    # return message
+    return nil unless valid_inputs?(args)
+
+    puts "Loading data: #{args}"
+    data = load_data(args[0])
+    changes = load_changes(args[1])
+    changed_data = change_data(data, changes)
+    save_output(changed_data, args[2])
   end
 
-  def self.load_data
-    file = File.read("spotify.json")
-    data = JSON.parse(file)
-    puts data.to_s
+  def self.change_data(data, changes)
     return data
   end
+
+  def self.load_data(filename)
+    JSON.parse(File.read(filename.to_s))
+  end
   
-  def self.load_changes
-    file = File.read("changes.json")
-    data = JSON.parse(file)
+  def self.load_changes(filename)
+    JSON.parse(File.read(filename.to_s))
   end
 
-  def self.save_output
-    json_string = JSON.pretty_generate(load_data)
-
-    output_file_path = "output.json"
-    File.open(output_file_path, "w") do |file|
-      file.write(json_string)
+  def self.save_output(data, filename)
+    File.open(filename, "w") do |file|
+      file.write(JSON.pretty_generate(data))
     end
+  end
+
+  def self.valid_inputs?(args)
+    message = ""
+    message += "Missing <input file>. "   if args[0].nil?
+    message += "Missing <changes file>. " if args[1].nil?
+    message += "Missing <output file>. "  if args[2].nil?
+    puts message if message.length >= 1
+    return message.length == 0
   end
 end
